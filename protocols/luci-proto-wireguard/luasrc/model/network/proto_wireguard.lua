1ac00728c22c02813435b6d93ba0002e26bac05f
DingYi <47165900@qq.com>
2024-03-22 21:06:40 +0800

    Update proto_wireguard.lua

--- protocols/luci-proto-wireguard/luasrc/model/network/proto_wireguard.lua
+++ protocols/luci-proto-wireguard/luasrc/model/network/proto_wireguard.lua
@@ -1,4 +1,4 @@
--- Copyright 2016 Dan Luedtke <mail@danrl.com>
+-- Copyright (C) 2018 DingYi <dingyi139@gmail.com>
 -- Licensed to the public under the Apache License 2.0.
 
 local netmod = luci.model.network
