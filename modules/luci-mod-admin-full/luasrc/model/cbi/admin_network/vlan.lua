9b2d32871c42c4a8aa5c9023bb8ae5f37d5131dc
DingYi <47165900@qq.com>
2024-03-22 20:59:59 +0800

    Update vlan.lua

--- modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/vlan.lua
+++ modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/vlan.lua
@@ -2,7 +2,7 @@
 -- Copyright 2010-2011 Jo-Philipp Wich <jow@openwrt.org>
 -- Licensed to the public under the Apache License 2.0.
 
-m = Map("network", translate("Switch"), translate("The network ports on this device can be combined to several <abbr title=\"Virtual Local Area Network\">VLAN</abbr>s in which computers can communicate directly with each other. <abbr title=\"Virtual Local Area Network\">VLAN</abbr>s are often used to separate different network segments. Often there is by default one Uplink port for a connection to the next greater network like the internet and other ports for a local network."))
+m = Map("network", translate("Switch"))
 
 local fs = require "nixio.fs"
 local ut = require "luci.util"
